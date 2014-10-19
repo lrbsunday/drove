e54a7a11b632c48d68c0822e73e6dbc92a6801e3
Andrés J. Díaz <ajdiaz@connectical.com>
2014-10-19 18:49:09 +0200

    Add more checks for coverage

--- test/test_daemon_posix.py
+++ test/test_daemon_posix.py
@@ -3,16 +3,21 @@
 # vim:fenc=utf-8
 
 import os
-from nose.tools import raises
-from nose.tools import with_setup
 from drove.daemon import Daemon
+from nose.tools import with_setup
 
 if os.name == "posix":
 
     _fork = os.fork
+    _kill = os.kill
 
+    def _tear_down():
+        os.fork = _fork
+        os.kill = _kill
+
+    @with_setup(teardown=_tear_down)
     def test_daemon_posix():
-        """Testing daemon.posix: basic behaviour"""
+        """Testing daemon.posix: parent behaviour"""
         d = Daemon.create(lambda: None, lambda: None)
         d.foreground()
         os.fork = lambda: 0
@@ -20,4 +25,12 @@ if os.name == "posix":
         d.restart()
         os.fork = _fork
 
-
+    @with_setup(teardown=_tear_down)
+    def test_daemon_posix_child():
+        """Testing daemon.posix: child behaviour"""
+        d = Daemon.create(lambda: None, lambda: None)
+        os.fork = lambda: 100
+        os.kill = lambda x, y: None
+        d.start()
+        assert d.pid == 100
+        d.stop()
