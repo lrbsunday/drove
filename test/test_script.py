609df7ff30df34720f6afea81781b29a16588244
Andrés J. Díaz <ajdiaz@connectical.com>
2014-10-20 10:38:26 +0200

    Add script test

--- test/test_script.py
+++ test/test_script.py
@@ -25,43 +25,11 @@ def test_script():
     """Testing script: basic behaviour"""
     sys.exit = lambda x: None
     sys.argv = ["prog", "-h"]
-    drove.script.CliScript()()
-    sys.argv = ["prog", "-v", "-C", "./test/config/empty.yml"]
-    drove.script.CliScript()()
+    drove.script.cli()
+    sys.argv = ["prog", "-np", "-v", "-C", "./test/config/empty.yml"]
     os.path.dirname = lambda x: ""
-    sys.argv = ["prog"]
-    drove.script.CliScript()()
+    sys.argv = ["prog", "-np"]
+    drove.script.cli()
+    sys.argv = ["prog", "-np", "-s", "key=value"]
+    drove.script.cli()
 
-
-@raises(ValueError)
-@with_setup(teardown=_teardown)
-def test_script_env_failed():
-    """Testing script: set wrong flag"""
-    sys.exit = lambda x: None
-    sys.argv = ["prog", "-v", "-s", "invalid"]
-    drove.script.CliScript()()
-
-
-class _MockLog(object):
-    def error(self, *args, **kwargs):
-        pass
-
-
-class _MockPlugins(object):
-    def stop_all(self, *args, **kwargs):
-        pass
-
-    def start_all(self, *args, **kwargs):
-        pass
-
-
-@with_setup(teardown=_teardown)
-def test_script_env_success():
-    """Testing script: set fine flag"""
-    sys.exit = lambda x: None
-    sys.argv = ["prog", "-v", "-s", "valid=true"]
-    sys.path.insert(0, "./importer")
-    x = drove.script.CliScript()
-    x.log = _MockLog()
-    x.plugins = _MockPlugins()
-    x._exit_handler()
